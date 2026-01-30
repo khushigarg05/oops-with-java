b6b7fb8d8e51c7525d8b06e9025dd4eb3a8912de
khushigarg05 <gargkhushi0511@gmail.com>
2026-01-30 10:22:24 +0530

    initial commit

---
+++ Area.java
@@ -0,0 +1,8 @@
+public class Area {
+    public static void main(String[] args) {
+        int a = 4;
+        double area = 3.14 * a * a;
+
+        System.out.println(area);
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
+*/
+
+public class Hello {
+    public static void main(String[] args) {
+        int a=10;
+        float b=3.45f;
+        double d =6.76;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println("The numbers are"+a+""+b+""+d);
+    }}
