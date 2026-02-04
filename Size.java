0db1f31fcb779a7fd009eb651426d531c60b098f
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-04 11:28:17 +0530

    size

---
+++ Size.java
@@ -0,0 +1,10 @@
+public class Size {
+    public static void main(String[] args) {
+        byte b;
+
+        System.out.println("Size of byte in bits: " + Byte.SIZE);
+        System.out.println("Size of byte in bytes: " + Byte.BYTES);
+        System.out.println("Minimum value of byte: " + Byte.MIN_VALUE);
+        System.out.println("Maximum value of byte: " + Byte.MAX_VALUE);
+    }
+}
