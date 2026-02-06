ce0128019ffbd5b7e1f1bf1b9b9e741e1971853d
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-06 09:57:06 +0530

    static method

---
+++ Main3.java
@@ -0,0 +1,16 @@
+class StaticMethod{
+    static void print(){
+        System.out.println(" i am in static method");
+
+    }
+}
+
+
+
+
+public class Main3 {
+    public static void main(String[] args) {
+        StaticMethod.print();
+    }
+    
+}
