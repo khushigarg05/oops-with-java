5f726e39a418c8d2452f13c8be54670a8d75a5d9
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-06 09:00:12 +0530

    class method

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Rectangle {
+    int length;
+    int breadth;
+
+    // Method to get data
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length: ");
+        length = sc.nextInt();
+
+        System.out.print("Enter breadth: ");
+        breadth = sc.nextInt();
+    }
+
+    // Method to calculate area
+    void calculateArea() {
+        int area = length * breadth;
+        System.out.println("Area of Rectangle = " + area);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.getData();
+        r.calculateArea();
+    }
+}
