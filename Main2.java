b79835d8886cedfcaf9af1daf7a7e95cab4eb8f9
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-06 09:40:06 +0530

    static instance

---
+++ Main2.java
@@ -0,0 +1,25 @@
+class Student{
+    int r;
+    String name;
+    static String college="ABES";
+    static int count=0;
+    void getdata(int r,String name){
+        this.r=r;
+        this.name=name;
+    }
+    void display(){
+        System.out.println(r);
+        System.out.println(name);
+        System.out.println(college);
+        System.out.println(count);
+    }
+}
+public class Main2 {
+    public static void main(String[] args) {
+        Student S = new Student();
+        S.getdata(1,"Khushi");
+        S.display();
+
+    }
+    
+}
