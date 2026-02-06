b56ef686604068bfa598b67404f6346f2b42f151
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-06 10:00:51 +0530

    calculator

---
+++ Main4.java
@@ -0,0 +1,20 @@
+class StaticMethod{
+    static int add(int x, int y){
+        return(x+y);
+    }
+    static int mul(int x ,int y){
+        return(x*y);
+
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        int res1=StaticMethod.add(10,20);
+        System.out.println(res1);
+        int res2=StaticMethod.mul(10,20);
+        System.out.println(res2);
+        
+    }
+    
+}
