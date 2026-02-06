7e1ac917c208526b8ded5812db4f4ff828b04acc
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-06 13:00:26 +0530

    constructor

---
+++ Main8.java
@@ -0,0 +1,17 @@
+class Default{
+    Default()
+    {
+        System.out.println("I am default");
+    }
+}
+
+
+
+
+public class Main8 {
+    public static void main(String[] args) {
+        Default D = new Default();
+        
+    }
+    
+}
