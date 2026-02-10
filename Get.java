50dfe4b479bddc13327b6f31127201c7dc063b08
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-10 09:13:32 +0530

    constructor

---
+++ Get.java
@@ -0,0 +1,16 @@
+class Demo1{
+    private int r;
+    public int getR
+
+}
+
+
+public class Get {
+    public static void main(String[] args) {
+        Demo1 D= new Demo1();
+        D.setR(20);
+        int res =D.getR();
+        System.out.println(res);
+    }
+    
+}
