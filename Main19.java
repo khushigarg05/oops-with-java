5d87e3887976d5faaed5b2697464c0ad2a287d92
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-12 11:26:57 +0530

    static updates

---
+++ Main19.java
@@ -0,0 +1,29 @@
+class Parent {
+
+    void show() {
+        System.out.println("Parent show method");
+    }
+
+    static void display() {
+        System.out.println("Parent static display method");
+    }
+}
+
+class Child extends Parent {
+
+    void show() {
+        System.out.println("Child show method");
+    }
+    static void display() {
+        System.out.println("Child static display method");
+    }
+}
+
+public class Main19 {
+    public static void main(String[] args) {
+
+        Parent p = new Child();
+        p.show();       
+        p.display();  
+    }
+}
