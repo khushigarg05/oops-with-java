28704af81b80a92b90a9904caf9ff5d2db64b0d7
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-24 09:08:18 +0530

    interface

---
+++ Program1.java
@@ -0,0 +1,23 @@
+interface I1{
+    int r=2;
+    String name="khushi";
+
+}
+
+interface I2 extends I1{
+    void display();
+
+}
+
+    
+public class Program1 implements I2 {
+    public void display(){
+        System.out.println("Roll NO:"+r);
+        System.out.println("Name:"+name);
+    }
+    public static void main(String[] args) {
+        Program1 P=new Program1();
+        P.display();
+    }
+    
+}
