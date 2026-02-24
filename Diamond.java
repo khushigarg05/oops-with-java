c44957a7f89ea2f0a99e9c3f47952c110268192c
khushigarg05 <gargkhushi0511@gmail.com>
2026-02-24 09:27:01 +0530

    diamomd

---
+++ Diamond.java
@@ -0,0 +1,29 @@
+interface AA{
+    default void display(){
+        System.out.println("Display in Interfacde AA");
+    }
+}
+interface BB extends AA{
+    default void display() {
+        System.out.println("Display in Interface BB");
+        
+    }
+}
+interface CC extends AA{
+    default void display() {
+        System.out.println("Display in Interface CC");
+    }
+}
+
+class Program2 implements BB, CC{
+    public void display(){
+        System.out.println("Display in Class Program2");
+    }
+}
+public class Diamond {
+    public static void main(String[] args) {
+        Program2 P2=new Program2();
+        P2.display();
+    }
+    
+}
